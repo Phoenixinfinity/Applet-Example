2d5340dc905b9a0ade9806345a721e897e503b0e
MITALI BOSE <73990307+Phoenixinfinity@users.noreply.github.com>
2020-11-25 21:45:43 +0530

    Add files via upload

---
+++ src/applet/Applet.java
@@ -0,0 +1,10 @@
+package applet;
+import java.applet.*;
+import java.awt.*;
+public class Applet extends Applet {
+	public void paint(Graphics g)
+	{
+		g.drawString("Hello World!!",30,30);
+	}
+
+}
